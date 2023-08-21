d18e44b3cd5394a5a5b30be05ebebf362a459a9d
Hassan Raza <141661553+HassanRaza-hr@users.noreply.github.com>
2023-08-21 17:05:32 +0500

    Add files via upload

---
+++ assign02.ts
@@ -0,0 +1,5 @@
+// Implement a simple shopping cart program using an array. Create functions to add items, remove items, and update quantities
+// using the splice method. Print the cart's contents after each operation
+var array:string[]=["perfume","oil","t-shirs"]
+array.splice(3,0,"noodles")
+console.log(array)
